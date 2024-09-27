88541f63ef028f293967210de3569ffc315faf4f
Markus <markus.vickman@hotmail.com>
2024-09-27 13:39:47 +0200

    lagt till variabler för startknapp och för rättsvar. Samt skapat metoder för rättsvar och för entry av svar. samt variabel för totalpoäng

--- TimedMath/MainPage.xaml.cs
+++ TimedMath/MainPage.xaml.cs
@@ -1,15 +1,36 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+
+//using Windows.ApplicationModel.Calls;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace TimedMath
 {
     public partial class MainPage : ContentPage
     {
 
+      /*  public class Player
+        {
 
-        private static Timer timer;
+            public string User { get; set; }
+            public string Post { get; set; }
+
+            //Konstruktorn som skapar gästinläggs-objekt
+            public Player(string user, string post)
+            {
+                User = user;
+                Post = post;
+            }
+        }*/
+
+        public string answer;
+        public int totalPoints;
+
+        //private static Timer timer;
 
 
         int count = 0;
@@ -18,21 +39,101 @@ namespace TimedMath
         {
             InitializeComponent();
 
+            Entry entry = new Entry { Placeholder = "Enter text" };
+            entry.TextChanged += OnEntryTextChanged!;
+            entry.Completed += OnEntryCompleted!;
 
         }
 
-        private static System.Timers.Timer aTimer;
-        public event System.Timers.ElapsedEventHandler Elapsed;
-       
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
         {
-            ChangeLabel();
-            Task.Delay(5000).Wait();
+            string text = ((Entry)sender).Text;
+            // Handle the text changed event here
+            if (text == answer && checkIfPressed)
+            {
+                totalPoints++;
+                ChangeLabel();
+                ((Entry)sender).Text = "";
+            }
+        }
 
-            CounterBtn.Text = "Nu har det gått 5 sekunder";
+        private void OnEntryCompleted(object sender, EventArgs e)
+        {
+
+            // Handle the completed event here
+        }
+
+        //private static System.Timers.Timer aTimer;
+        // public event System.Timers.ElapsedEventHandler Elapsed;
+
+        public bool checkIfPressed = false;
+
+        public async void OnCounterClicked(object sender, EventArgs e)
+        {
+
+            if (!checkIfPressed)
+            {
+                /*CancellationTokenSource source = new CancellationTokenSource();
+                CancellationToken token = source.Token;
+                */
+                totalPoints = 0;
+                CounterBtn.Text = "Stopp";
+                checkIfPressed = true;
+                ChangeLabel();
+                await Task.Delay(30000/*, token*/);
+
+            }
+
+            // Player player1 = new Player("test", "guestPost");
+
+            CounterBtn.Text = "Start";
+
+            //Task.
+
+            Label ansLabel = (Label)FindByName("question");
+            ansLabel.Text = "Total points: " + totalPoints;
+            checkIfPressed = false;
+            }
+
+        public void OnSkipClicked(object sender, EventArgs e)
+        {
+            if (checkIfPressed)
+            {
+                ChangeLabel();
+            }
+
+            // Player player1 = new Player("test", "guestPost");
+
+        
+        }
+
+
+        private void SubmitAnswerClicked(object sender, EventArgs e)
+        {
+
+            Entry ansLabel = (Entry)FindByName("entry");
+            string inputAnswer = ansLabel.Text;
+
+            // if (inputAnswer == ull ) {
 
 
         }
+        /*
+        public string RightAnswer(string answer, bool check)
+        {
+            string rightAnswer;
+
+            if (!check)
+            {
+              rightAnswer = answer; ;
+
+            }
+
+            
+            return rightAnswer;
+        }*/
+
+
 
         public void ChangeLabel()
         {
@@ -42,11 +143,18 @@ namespace TimedMath
             int num2 = rand.Next(51);
 
 
-            string retVal = $"{num1} + {num2} = {num1 + num2}";
+
+            answer = (num1 + num2).ToString();
+
+            string retVal = $"{num1} + {num2} = ?";
             Label ansLabel = (Label)FindByName("question");
             ansLabel.Text = retVal;
-        }
 
+            //bool check = false;
+
+            //RightAnswer(answer, check);
+
+        }
     }
 
-}
+    }
